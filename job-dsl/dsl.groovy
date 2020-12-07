574163db950d25f47be1ee6ab8c8b0515cc88bb8
aryelciu001 <aryelciu001@gmail.com>
2020-12-07 16:57:25 +0800

    edited dsl file

--- job-dsl/dsl.groovy
+++ job-dsl/dsl.groovy
@@ -1,9 +1,11 @@
 job('NodeJS example') {
+    // software configuration management
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
-        }
+        git('https://github.com/aryelciu001/jenkins-trial.git') 
+        // {  node -> // is hudson.plugins.git.GitSCM
+        //     // node / gitConfigName('DSL User')
+        //     // node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        // }
     }
     triggers {
         scm('H/5 * * * *')
